3a501f37f47789711efdd38dbaee2c8b293744a5
SrvK21_ <vinayaksaurav284@gmail.com>
2024-12-24 17:36:57 +0530

    rendered product successfully

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Hero from '../components/Hero'
+import LatestCollection from '../components/latestCollection'
+
+
 
 const Home = () => {
   return (
     <div>
-      
+        {/* hero component has mounted on home page */}
+       
+     <Hero/>
+     <LatestCollection/>
     </div>
   )
 }
