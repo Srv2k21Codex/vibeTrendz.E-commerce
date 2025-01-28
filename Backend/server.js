3881c65d9adc5c1c19725adb938a27536610c12e
SrvK21_ <vinayaksaurav284@gmail.com>
2025-01-28 11:26:31 +0530

    backend setup and api testing completed

--- Backend/server.js
+++ Backend/server.js
@@ -1,16 +1,25 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js';
+import userRouter from './routes/userRoute.js';
+import productRoute from './routes/productRoute.js';
+
 
 //app config
 const app = express();
 const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
 
 //middlewares
 app.use(express.json())
 app.use(cors())
 
 //api endpoints 
+app.use('/api/user',userRouter)
+app.use('/api/product',productRoute)
 
 app.get('/',(req,res)=>{
     res.send("API working")//whenever we will open port 4000 it will show api working message
